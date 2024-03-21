32fcf835abf278730f626e442d89dec940a24b33
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-03-21 15:33:53 +0100

    feat: add createForecast

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ const db = {
   temperature: ['15°C with a cool breeze', '20°C with a mild breeze', '25°C with comfortable temperatures', '30°C with a heatwave warning', '35°C with scorching temperatures'],
   windSpeed: ['5 km/h winds with light breezes', '10 km/h winds with occasional gusts', '15 km/h winds with moderate breezes', '20 km/h winds with strong gusts', '25 km/h winds with sustained breezes']
 };
+
+function createForecast() {
+  const forecast = [];
+
+  for (const key in db) {
+    let index = Math.floor(Math.random() * db[key].length);
+
+    switch (key) {
+      case 'cloudCoverage':
+        forecast.push(`Today's forecast is ${db[key][index]}.`)
+        break;
+      case 'temperature':
+        forecast.push(`The temperature is ${db[key][index]} and`)
+        break;
+      case 'windSpeed':
+        forecast.push(`${db[key][index]}.`)
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return forecast.join(' ');
+}
+
+console.log(createForecast())
