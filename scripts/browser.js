b13001cfcab775b0e5fb1c2abed18c7f423876f0
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-03-25 13:54:03 +0100

    refa: breakout styles

--- scripts/browser.js
+++ scripts/browser.js
@@ -1,5 +1,6 @@
 import { wrapText, createForecast } from "./shared.js";
 import { createElementHelper } from "./helpers.js";
+import { applyStyles } from "./styles.js";
 
 function createAsciiArt(condition) {
 
@@ -47,50 +48,17 @@ function formatForecast() {
 
 function formatDocument() {
   const mainEl = createElementHelper('main');
-  document.body.insertBefore(mainEl, document.body.firstChild);
-
   const sectionEl = createElementHelper('section');
-  document.querySelector('main').appendChild(sectionEl);
-
   const hEl = createElementHelper('h1', 'Mixed Messages');
-  document.querySelector('section').appendChild(hEl);
-
   const divEl = createElementHelper('div');
+
+  document.body.insertBefore(mainEl, document.body.firstChild);
+  document.querySelector('main').appendChild(sectionEl);
+  document.querySelector('section').appendChild(hEl);
   document.querySelector('section').appendChild(divEl);
 
   formatForecast()
-
-  const allEl = document.querySelectorAll('*');
-  allEl.forEach((el) => {
-    el.style.boxSizing = 'border-box';
-    el.style.margin = '0';
-    el.style.padding = '0';
-    el.style.minWidth = '0';
-  });
-
-  const htmlEl = document.querySelector('html');
-  htmlEl.style.fontFamily = '"Roboto", sans-serif';
-
-  hEl.style.textAlign = 'center';
-  hEl.style.padding = '25px';
-  hEl.style.fontSize = '30px';
-  hEl.style.lineHeight = '36px';
-  hEl.style.fontWeight = '700';
-
-  divEl.style.margin = '0 auto';
-  divEl.style.padding = '9px';
-  divEl.style.color = '#C5C5C5';
-  divEl.style.backgroundColor = '#1E1E1E';
-  divEl.style.width = '1035px';
-  divEl.style.height = '635px'
-  divEl.style.borderRadius = '10px';
-  divEl.style.boxShadow = '0 25px 50px -12px rgb(0 0 0 / 0.25)';
-
-  const preEls = document.querySelectorAll('pre');
-  preEls.forEach(preEl => {
-    preEl.style.fontFamily = '"MonoLisa", sans-serif';
-    preEl.style.fontSize = '12px'
-  });
+  applyStyles()
 }
 
 formatDocument()
---
+++ scripts/styles.js
@@ -0,0 +1,35 @@
+export function applyStyles() {
+  const allEl = document.querySelectorAll('*');
+  allEl.forEach((el) => {
+    el.style.boxSizing = 'border-box';
+    el.style.margin = '0';
+    el.style.padding = '0';
+    el.style.minWidth = '0';
+  });
+
+  const htmlEl = document.querySelector('html');
+  htmlEl.style.fontFamily = '"Roboto", sans-serif';
+
+  const hEl = document.querySelector('h1');
+  hEl.style.textAlign = 'center';
+  hEl.style.padding = '25px';
+  hEl.style.fontSize = '30px';
+  hEl.style.lineHeight = '36px';
+  hEl.style.fontWeight = '700';
+
+  const divEl = document.querySelector('div');
+  divEl.style.margin = '0 auto';
+  divEl.style.padding = '9px';
+  divEl.style.color = '#C5C5C5';
+  divEl.style.backgroundColor = '#1E1E1E';
+  divEl.style.width = '1035px';
+  divEl.style.height = '635px'
+  divEl.style.borderRadius = '10px';
+  divEl.style.boxShadow = '0 25px 50px -12px rgb(0 0 0 / 0.25)';
+
+  const preEls = document.querySelectorAll('pre');
+  preEls.forEach(preEl => {
+    preEl.style.fontFamily = '"MonoLisa", sans-serif';
+    preEl.style.fontSize = '12px'
+  });
+}
