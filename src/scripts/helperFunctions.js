aa00b63c5b3a5e57bf6323d804e28774f9612e7f
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-04-05 17:10:57 +0200

    refa: accept multiple attributes

--- src/scripts/helperFunctions.js
+++ src/scripts/helperFunctions.js
@@ -1,8 +1,9 @@
-export function createElementHelper(elName, elValue, attrName, attrValue) {
+export function createElementHelper(elName, attrName, attrValue, attrName2, attrValue2, attrName3, attrValue3) {
   const el = document.createElement(elName);
 
   if (attrName && attrValue) el.setAttribute(attrName, attrValue)
-  if (elValue) el.textContent = elValue;
+  if (attrName2 && attrValue2) el.setAttribute(attrName2, attrValue2)
+  if (attrName3 && attrValue3) el.setAttribute(attrName3, attrValue3)
 
   return el;
 }
