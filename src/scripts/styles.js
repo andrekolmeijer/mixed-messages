813614e661b873c738c5f96dddb0a6d3bf23d7f5
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-04-14 12:14:14 +0200

    refa: extract loadFonts

--- src/scripts/styles.js
+++ src/scripts/styles.js
@@ -1,28 +1,4 @@
-async function loadFonts() {
-  const plexMonoRegular = new FontFace('IBM Plex Mono', 'url(src/assets/fonts/IBMPlexMono-Regular.woff2) format(woff2)', {
-    weight: 400,
-    style: 'normal',
-    display: 'swap'
-  });
-  const plexMonoSemiBold = new FontFace('IBM Plex Mono', 'url(src/assets/fonts/IBMPlexMono-SemiBold.woff2) format(woff2)', {
-    weight: 600,
-    style: 'normal',
-    display: 'swap'
-  });
-  const openSansVariable = new FontFace('Open Sans', 'url(src/assets/fonts/OpenSans-VariableFont.woff2) format("woff2-variations")', {
-    weight: '300 800',
-    style: 'normal',
-    display: 'swap'
-  });
-
-  await plexMonoRegular.load();
-  await plexMonoSemiBold.load();
-  await openSansVariable.load();
-
-  document.fonts.add(plexMonoRegular);
-  document.fonts.add(plexMonoSemiBold);
-  document.fonts.add(openSansVariable);
-}
+import { loadFonts } from "../util/loadFonts.js";
 
 export function applyStyles() {
 
