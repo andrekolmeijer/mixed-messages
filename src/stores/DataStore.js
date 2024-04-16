a1315d2113b9e727eaf078301b935f7bb1b9e9c5
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-04-16 12:19:08 +0200

    refa: use data store

--- src/stores/DataStore.js
+++ src/stores/DataStore.js
@@ -1,3 +1,22 @@
 export const useDataStore = () => ({
-  author: { name: 'André Kolmeijer' }
+  author: {
+    name: 'André Kolmeijer',
+    website: 'https://andrekolmeijer.nl',
+    profile: 'andrekolmeijer',
+    repo: 'mixed-messages'
+  },
+  provider: {
+    github: 'https://github.com',
+    nodejs: 'https://nodejs.org',
+    twitter: 'https://x.com',
+    linkedin: 'https://linkedin.com'
+  },
+  get url() {
+    return {
+      'github-repo': `${this.provider.github}/${this.author.profile}/${this.author.repo}`,
+      'github-profile': `${this.provider.github}/${this.author.profile}`,
+      'twitter-profile': `${this.provider.twitter}/${this.author.profile}`,
+      'linkedin-profile': `${this.provider.linkedin}/in/${this.author.profile}`,
+    }
+  }
 });
