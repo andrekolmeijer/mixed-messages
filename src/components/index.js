89db51216b968d364c56c52f6c5ea25e4eac92ee
Andre Kolmeijer <19167112+andrekolmeijer@users.noreply.github.com>
2024-04-27 10:24:03 +0200

    refa: extract predict section

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
+export { default as PredictSection } from './common/PredictSection.js';
+export { default as ConsoleSection } from './common/ConsoleSection.js';
+
 export { default as NavBar } from './containers/NavBar.js';
 export { default as Footer } from './containers/Footer.js';
-
-export { default as ConsoleSection } from './common/ConsoleSection.js';
